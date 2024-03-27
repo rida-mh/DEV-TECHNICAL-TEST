af1d3092ed23e23e537c37d73f523fbd1dddee7f
Alexandre Gauit <alexandre.gazuit@enera.fr>
2024-03-27 23:01:02 +0100

    Feat - Implement dummy LocalStorage ORM

---
+++ src/Core/Entities/BaseEntity.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace EneraTechTest\Core\Entities;
+
+use JsonSerializable;
+use ReflectionObject;
+use ReflectionProperty;
+
+abstract class BaseEntity implements JsonSerializable
+{
+    public function jsonSerialize(): mixed
+    {
+        $reflection = new ReflectionObject($this);
+        $properties = $reflection->getProperties(
+            ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE
+        );
+
+        $data = [];
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $data[$property->getName()] = $property->getValue($this);
+        }
+
+        return $data;
+    }
+}
---
+++ src/Infrastructure/Database/LocalFileDBContext.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace EneraTechTest\Infrastructure\Database;
+
+use ReflectionClass;
+use EneraTechTest\Core\Entities\BaseEntity;
+
+class LocalFileDBContext
+{
+    private string $storagePath = __DIR__ . '/Data';
+    private array $data = [];
+
+    public function __construct()
+    {
+        $this->loadData();
+    }
+
+    private function loadData(): void
+    {
+        $this->data = [];
+
+        foreach (glob($this->storagePath . '/*.json') as $file) {
+
+            $fileName = basename($file, '.json');
+            $fullClassName = str_replace('_', '\\', $fileName);
+
+            $jsonData = file_get_contents($file);
+            $entitiesArray = json_decode($jsonData, true);
+
+            $entities = [];
+            foreach ($entitiesArray as $entityData) {
+                $entities[] = $this->jsonDeserialize($entityData, $fullClassName);
+            }
+
+            $this->data[$fullClassName] = $entities;
+        }
+    }
+
+    public function flush(): void
+    {
+        foreach ($this->data as $fullClassName => $entities) {
+
+            $fileName = str_replace('\\', '_', $fullClassName) . '.json';
+            $filePath = $this->storagePath . '/' . $fileName;
+
+            $jsonData = json_encode($entities);
+            file_put_contents($filePath, $jsonData);
+        }
+    }
+
+    private function jsonDeserialize(array $data, string $className): object
+    {
+        $reflector = new ReflectionClass($className);
+
+        $object = $reflector->newInstanceWithoutConstructor();
+
+        foreach ($data as $key => $value) {
+            if ($reflector->hasProperty($key)) {
+                $property = $reflector->getProperty($key);
+                $property->setAccessible(true);
+                $property->setValue($object, $value);
+            }
+        }
+
+        return $object;
+    }
+
+    public function persist(BaseEntity $entity): void
+    {
+        $fullClassName = get_class($entity);
+
+        if (!isset($this->data[$fullClassName])) {
+            $this->data[$fullClassName] = [];
+        }
+
+        $this->data[$fullClassName][] = $entity;
+    }
+
+    public function get(string $entityClassName): array
+    {
+
+        if (!isset($this->data[$entityClassName])) {
+            return [];
+        }
+
+        return $this->data[$entityClassName];
+    }
+}
